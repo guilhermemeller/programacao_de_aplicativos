04feb3a72c86921210ef9174ec58e1826693f1fa
ChudsV <jvchudek90@gmail.com>
2023-05-23 15:48:24 -0300

    Update entities

---
+++ MSMoney/src/entities/Financa.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Financa {
+	private String nome;
+	private String categoria;
+	private boolean mensal_Ocasional;
+	private Double total;
+}
--- MSMoney/src/entities/Usuario.java
+++ MSMoney/src/entities/Usuario.java
@@ -1,6 +1,24 @@
 package entities;
 
+import java.util.List;
+
 public class Usuario {
+	
 	private String login;
 	private String senha;
+	private String nome;
+	
+	private List<Financa> rendimentos;
+	private List<Financa> despesas;
+	private List<Financa> longoPrazo;
+	private List<Financa> despesasOcasionais;
+	
+	private List<List<Financa>> resumo;
+	
+	public Usuario() {
+		
+	}
+	
+	
+	
 }
