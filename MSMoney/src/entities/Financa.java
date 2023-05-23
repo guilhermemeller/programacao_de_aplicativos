bf54aa956e96d5f45a1281b5ed62737efc23aa1a
ChudsV <jvchudek90@gmail.com>
2023-05-23 20:12:26 -0300

    Update entities

--- MSMoney/src/entities/Financa.java
+++ MSMoney/src/entities/Financa.java
@@ -5,4 +5,60 @@ public class Financa {
 	private String categoria;
 	private boolean mensal_Ocasional;
 	private Double total;
+	private FinancaEnum tipo;
+	
+	public enum FinancaEnum {
+		REDIMENTO,
+		DESPESAS,
+		LONGO_PRAZO,
+		DESPESAS_OCASIONAIS
+	}
+	
+	public Financa(String nome, String categoria, boolean mensal_Ocasional, Double total, FinancaEnum tipo) {
+		this.nome = nome;
+		this.categoria = categoria;
+		this.mensal_Ocasional = mensal_Ocasional;
+		this.total = total;
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public boolean isMensal_Ocasional() {
+		return mensal_Ocasional;
+	}
+
+	public void setMensal_Ocasional(boolean mensal_Ocasional) {
+		this.mensal_Ocasional = mensal_Ocasional;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	@Override
+	public String toString() {
+		return "Financa [nome=" + nome + ", categoria=" + categoria + ", mensal_Ocasional=" + mensal_Ocasional
+				+ ", total=" + total + "]";
+	}
+	
+	
 }
--- MSMoney/src/entities/Usuario.java
+++ MSMoney/src/entities/Usuario.java
@@ -1,20 +1,103 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import entities.utils.UsuarioUtils;
+
 
 public class Usuario {
 	
 	private String login;
 	private String senha;
 	private String nome;
+	public UsuarioUtils usuarioUtils;
 	
 	private List<Financa> rendimentos;
 	private List<Financa> despesas;
 	private List<Financa> longoPrazo;
 	private List<Financa> despesasOcasionais;
 	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Financa> getRendimentos() {
+		return rendimentos;
+	}
+
+	public void setRendimentos(List<Financa> rendimentos) {
+		this.rendimentos = rendimentos;
+	}
+
+	public List<Financa> getDespesas() {
+		return despesas;
+	}
+
+	public void setDespesas(List<Financa> despesas) {
+		this.despesas = despesas;
+	}
+
+	public List<Financa> getLongoPrazo() {
+		return longoPrazo;
+	}
+
+	public void setLongoPrazo(List<Financa> longoPrazo) {
+		this.longoPrazo = longoPrazo;
+	}
+
+	public List<Financa> getDespesasOcasionais() {
+		return despesasOcasionais;
+	}
+
+	public void setDespesasOcasionais(List<Financa> despesasOcasionais) {
+		this.despesasOcasionais = despesasOcasionais;
+	}
+
+	public List<List<Financa>> getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(List<List<Financa>> resumo) {
+		this.resumo = resumo;
+	}
+
 	private List<List<Financa>> resumo;
 	
+	
+	
+	public Usuario(String login, String senha, String nome) {
+		this.login = login;
+		this.senha = senha;
+		this.nome = nome;
+		this.rendimentos = new ArrayList<Financa>();
+		this.despesas = new ArrayList<Financa>();
+		this.longoPrazo = new ArrayList<Financa>();
+		this.despesasOcasionais = new ArrayList<Financa>();
+		this.resumo = new ArrayList<List<Financa>>();
+		this.usuarioUtils = new UsuarioUtils();
+	}
+
 	public Usuario() {
 		
 	}
