cc667c7be4a38e58c5cf607a54fd37866f6e764e
ChudsV <jvchudek90@gmail.com>
2023-05-24 16:22:18 -0300

    Updata UsuarioTest.java

--- MSMoney/src/test/UsuarioTest.java
+++ MSMoney/src/test/UsuarioTest.java
@@ -8,24 +8,32 @@ import entities.Usuario;
 public class UsuarioTest {
 	public static void main(String[] args) {
 		Usuario usuario = new Usuario("jvchudek90@gmail.com", "1234", "João Chudek");
-		Financa financa = new Financa("Salario Mensal", "Salário", true, 1500.0,FinancaEnum.REDIMENTO,1);
+		
+		Financa financa = new Financa("Salario Mensal", "Salário", true, 1500.0,FinancaEnum.REDIMENTO);
 		usuario.setRendimentos(usuario.usuarioUtils.inserirFinanca(usuario.getRendimentos(), financa));
 		
-		Financa financa2 = new Financa("13 Salario", "Salário", false, 1500.0,FinancaEnum.REDIMENTO,1);
+		
+		Financa financa2 = new Financa("13 Salario", "Salário", false, 1500.0,FinancaEnum.REDIMENTO,2);
 		usuario.setRendimentos(usuario.usuarioUtils.inserirFinanca(usuario.getRendimentos(), financa2));
 		
-		Financa financa3 = new Financa("Combustivel","Automovel", true, 400.0,FinancaEnum.DESPESAS,1);
-		usuario.setDespesas(usuario.usuarioUtils.inserirFinanca(usuario.getDespesas(), financa3));
 		
-		Financa financa4 = new Financa("Poupança", true, 500.0,FinancaEnum.LONGO_PRAZO,1);
+		Financa financa3 = new Financa("Combustivel","Automovel", true, 400.0,FinancaEnum.DESPESAS);
+		usuario.setDespesas(usuario.usuarioUtils.inserirFinanca(usuario.getDespesas(), financa3));
+	
+		Financa financa4 = new Financa("Poupança", true, 500.0,FinancaEnum.LONGO_PRAZO);
 		usuario.setLongoPrazo(usuario.usuarioUtils.inserirFinanca(usuario.getLongoPrazo(), financa4));
 		
+		Financa financa5 = new Financa("IPVA", true, 300.0,FinancaEnum.DESPESAS_OCASIONAIS,1);
+		usuario.setDespesasOcasionais(usuario.usuarioUtils.inserirFinanca(usuario.getDespesasOcasionais(), financa5));
 		
-		usuario.usuarioUtils.vizualizarListaFinanca(usuario.getRendimentos());
-		usuario.usuarioUtils.vizualizarListaFinanca(usuario.getDespesas());
-		usuario.usuarioUtils.vizualizarListaFinanca(usuario.getLongoPrazo());
+		//usuario.usuarioUtils.vizualizarListaFinanca(usuario.getRendimentos());
+		//usuario.usuarioUtils.vizualizarListaFinanca(usuario.getDespesas());
+		//usuario.usuarioUtils.vizualizarListaFinanca(usuario.getLongoPrazo());
 		
-		usuario.setResumo(usuario.usuarioUtils.atualizarResumo(usuario.getResumo(), usuario.getRendimentos(), usuario.getDespesas(), usuario.getLongoPrazo()));
-		usuario.usuarioUtils.vizualizarResumo(usuario.getResumo());
+		usuario.setResumo(usuario.usuarioUtils.atualizarResumo(usuario.getResumo(), usuario.getRendimentos(), usuario.getDespesas(), usuario.getLongoPrazo(),usuario.getDespesasOcasionais()));
+		for(int i=0;i<12;i++) {
+			usuario.usuarioUtils.vizualizarResumoMes(usuario.getResumo(),i+1);
+		}
+		
 	}
 }
