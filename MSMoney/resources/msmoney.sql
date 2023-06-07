64020193f2048ebb333e25f849bca11e533cc102
Matheus <matheus-mitsrael@outlook.com>
2023-06-07 18:37:53 -0300

    Update database tables

--- MSMoney/resources/msmoney.sql
+++ MSMoney/resources/msmoney.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06/06/2023 às 20:50
+-- Tempo de geração: 07/06/2023 às 23:36
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -35,16 +35,45 @@ CREATE TABLE `categoria` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `financa`
+-- Estrutura para tabela `fundo_despesas`
 --
 
-CREATE TABLE `financa` (
+CREATE TABLE `fundo_despesas` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `nome` int(11) NOT NULL,
+  `total` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `investimento`
+--
+
+CREATE TABLE `investimento` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `nome` varchar(240) NOT NULL,
+  `mensal_ocasional` tinyint(1) NOT NULL,
+  `total` double(10,2) NOT NULL,
+  `tipo` varchar(240) NOT NULL,
+  `mes` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `rendimento_despesa`
+--
+
+CREATE TABLE `rendimento_despesa` (
   `id` int(11) NOT NULL,
   `usuario_id` int(240) NOT NULL,
   `nome` varchar(240) NOT NULL,
   `categoria` int(11) NOT NULL,
   `mensal_ocasional` tinyint(1) NOT NULL,
-  `total` decimal(10,2) NOT NULL,
+  `total` double(10,2) NOT NULL,
   `tipo` varchar(240) NOT NULL,
   `mes` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -63,14 +92,6 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Despejando dados para a tabela `usuario`
---
-
-INSERT INTO `usuario` (`id`, `login`, `senha`, `nome`) VALUES
-(1, 'jvchudek90@gmail.com', '1234', 'João Chudek'),
-(3, 'matheus@mitz.com', '12345', 'Matheus Mitz');
-
---
 -- Índices para tabelas despejadas
 --
 
@@ -81,9 +102,23 @@ ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id_categoria`);
 
 --
--- Índices de tabela `financa`
+-- Índices de tabela `fundo_despesas`
 --
-ALTER TABLE `financa`
+ALTER TABLE `fundo_despesas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario_fk` (`usuario_id`);
+
+--
+-- Índices de tabela `investimento`
+--
+ALTER TABLE `investimento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario_fk` (`usuario_id`);
+
+--
+-- Índices de tabela `rendimento_despesa`
+--
+ALTER TABLE `rendimento_despesa`
   ADD PRIMARY KEY (`id`),
   ADD KEY `usuario_id_fk` (`usuario_id`),
   ADD KEY `id_categoria_fk` (`categoria`);
@@ -102,30 +137,54 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT de tabela `financa`
+-- AUTO_INCREMENT de tabela `fundo_despesas`
 --
-ALTER TABLE `financa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+ALTER TABLE `fundo_despesas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `investimento`
+--
+ALTER TABLE `investimento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de tabela `rendimento_despesa`
+--
+ALTER TABLE `rendimento_despesa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para tabelas despejadas
 --
 
 --
--- Restrições para tabelas `financa`
+-- Restrições para tabelas `fundo_despesas`
 --
-ALTER TABLE `financa`
-  ADD CONSTRAINT `financa_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `financa_ibfk_2` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `fundo_despesas`
+  ADD CONSTRAINT `fundo_despesas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `investimento`
+--
+ALTER TABLE `investimento`
+  ADD CONSTRAINT `investimento_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `rendimento_despesa`
+--
+ALTER TABLE `rendimento_despesa`
+  ADD CONSTRAINT `rendimento_despesa_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rendimento_despesa_ibfk_2` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
