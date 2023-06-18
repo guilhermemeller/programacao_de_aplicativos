d301947e9ffad3995c558231b0177f9d9f0ce0bc
Guilherme Portela Meller <guilhermeportelameller@gmail.com>
2023-06-17 23:46:31 -0300

    Updated Entrega Trabalho

---
+++ MSMoney/resources/DataBase/msmoney.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 18/06/2023 às 04:41
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `msmoney`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `nome_categoria` varchar(240) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `usuario_id` int(240) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fundo_despesas`
+--
+
+CREATE TABLE `fundo_despesas` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `nome` varchar(240) NOT NULL,
+  `total` double(10,2) NOT NULL,
+  `tipo` varchar(240) NOT NULL,
+  `mes` int(11) NOT NULL,
+  `ano` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `investimento`
+--
+
+CREATE TABLE `investimento` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `nome` varchar(240) NOT NULL,
+  `mensal_ocasional` tinyint(1) NOT NULL,
+  `total` double(10,2) NOT NULL,
+  `tipo` varchar(240) NOT NULL,
+  `mes` int(11) NOT NULL,
+  `ano` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `rendimento_despesa`
+--
+
+CREATE TABLE `rendimento_despesa` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(240) NOT NULL,
+  `nome` varchar(240) NOT NULL,
+  `categoria` int(11) NOT NULL,
+  `mensal_ocasional` tinyint(1) NOT NULL,
+  `total` double(10,2) NOT NULL,
+  `tipo` varchar(240) NOT NULL,
+  `mes` int(11) NOT NULL,
+  `ano` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `login` varchar(240) NOT NULL,
+  `senha` varchar(240) NOT NULL,
+  `nome` varchar(240) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`),
+  ADD KEY `usuario_id_fk` (`usuario_id`);
+
+--
+-- Índices de tabela `fundo_despesas`
+--
+ALTER TABLE `fundo_despesas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario_fk` (`usuario_id`);
+
+--
+-- Índices de tabela `investimento`
+--
+ALTER TABLE `investimento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario_fk` (`usuario_id`);
+
+--
+-- Índices de tabela `rendimento_despesa`
+--
+ALTER TABLE `rendimento_despesa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id_fk` (`usuario_id`),
+  ADD KEY `id_categoria_fk` (`categoria`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT de tabela `fundo_despesas`
+--
+ALTER TABLE `fundo_despesas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=217;
+
+--
+-- AUTO_INCREMENT de tabela `investimento`
+--
+ALTER TABLE `investimento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=217;
+
+--
+-- AUTO_INCREMENT de tabela `rendimento_despesa`
+--
+ALTER TABLE `rendimento_despesa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=803;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `categoria`
+--
+ALTER TABLE `categoria`
+  ADD CONSTRAINT `categoria_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `fundo_despesas`
+--
+ALTER TABLE `fundo_despesas`
+  ADD CONSTRAINT `fundo_despesas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `investimento`
+--
+ALTER TABLE `investimento`
+  ADD CONSTRAINT `investimento_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `rendimento_despesa`
+--
+ALTER TABLE `rendimento_despesa`
+  ADD CONSTRAINT `rendimento_despesa_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rendimento_despesa_ibfk_2` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
